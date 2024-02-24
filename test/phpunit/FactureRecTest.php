8ac0169cd9bb8059ae0fcf25a37b76896d771f55
rsanjoseo <info@rsanjoseo.com>
2024-02-24 19:03:41 +0100

    Restored temporarily deleted folders to lighten the repo size for Scrutinizer

---
+++ test/phpunit/FactureRecTest.php
@@ -0,0 +1,154 @@
+<?php
+/* Copyright (C) 2013 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2023 Alexandre Janniaux   <alexandre.janniaux@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * or see https://www.gnu.org/
+ */
+
+/**
+ *      \file       test/phpunit/FactureRecTest.php
+ *		\ingroup    test
+ *      \brief      PHPUnit test
+ *		\remarks	To run this script as CLI:  phpunit filename.php
+ */
+
+global $conf,$user,$langs,$db;
+//define('TEST_DB_FORCE_TYPE','mysql');	// This is to force using mysql driver
+//require_once 'PHPUnit/Autoload.php';
+require_once dirname(__FILE__).'/../../htdocs/master.inc.php';
+require_once dirname(__FILE__).'/../../htdocs/compta/facture/class/facture.class.php';
+require_once dirname(__FILE__).'/../../htdocs/compta/facture/class/facture-rec.class.php';
+require_once dirname(__FILE__).'/CommonClassTest.class.php';
+
+if (empty($user->id)) {
+	print "Load permissions for admin user nb 1\n";
+	$user->fetch(1);
+	$user->getrights();
+}
+$conf->global->MAIN_DISABLE_ALL_MAILS = 1;
+
+
+/**
+ * Class for PHPUnit tests
+ *
+ * @backupGlobals disabled
+ * @backupStaticAttributes enabled
+ * @remarks	backupGlobals must be disabled to have db,conf,user and lang not erased.
+ */
+class FactureRecTest extends CommonClassTest
+{
+	/**
+	 * testFactureRecCreate
+	 *
+	 * @return int
+	 */
+	public function testFactureRecCreate()
+	{
+		global $conf,$user,$langs,$db;
+
+		$conf = $this->savconf;
+		$user = $this->savuser;
+		$langs = $this->savlangs;
+		$db = $this->savdb;
+
+		$localobjectinv = new Facture($db);
+		$localobjectinv->initAsSpecimen();
+		$result = $localobjectinv->create($user);
+
+		print __METHOD__." result=".$result."\n";
+
+		$localobject = new FactureRec($db);
+		$localobject->initAsSpecimen();
+		$result = $localobject->create($user, $localobjectinv->id);
+
+		print __METHOD__." result=".$result."\n";
+		$this->assertGreaterThan(0, $result, 'Create recurring invoice from common invoice: '.$localobject->error);
+
+		return $result;
+	}
+
+	/**
+	 * testFactureRecFetch
+	 *
+	 * @param  int 	$id  	Id of created recuriing invoice
+	 * @return int
+	 *
+	 * @depends testFactureRecCreate
+	 * The depends says test is run only if previous is ok
+	 */
+	public function testFactureRecFetch($id)
+	{
+		global $conf,$user,$langs,$db;
+		$conf = $this->savconf;
+		$user = $this->savuser;
+		$langs = $this->savlangs;
+		$db = $this->savdb;
+
+		$localobject = new FactureRec($db);
+		$result = $localobject->fetch($id);
+
+		print __METHOD__." result=".$result."\n";
+		$this->assertGreaterThan(0, $result);
+		return $result;
+	}
+
+
+
+	/**
+	 * Edit an object to test updates
+	 *
+	 * @param 	FactureRec	$localobject		Object Facture rec
+	 * @return	void
+	 */
+	public function changeProperties(&$localobject)
+	{
+		$localobject->note_private = 'New note';
+		//$localobject->note='New note after update';
+	}
+
+	/**
+	 * Compare all public properties values of 2 objects
+	 *
+	 * @param 	Object		$oA						Object operand 1
+	 * @param 	Object		$oB						Object operand 2
+	 * @param	boolean		$ignoretype				False will not report diff if type of value differs
+	 * @param	array		$fieldstoignorearray	Array of fields to ignore in diff
+	 * @return	array								Array with differences
+	 */
+	public function objCompare($oA, $oB, $ignoretype = true, $fieldstoignorearray = array('id'))
+	{
+		$retAr = array();
+
+		if (get_class($oA) !== get_class($oB)) {
+			$retAr[] = "Supplied objects are not of same class.";
+		} else {
+			$oVarsA = get_object_vars($oA);
+			$oVarsB = get_object_vars($oB);
+			$aKeys = array_keys($oVarsA);
+			foreach ($aKeys as $sKey) {
+				if (in_array($sKey, $fieldstoignorearray)) {
+					continue;
+				}
+				if (! $ignoretype && ($oVarsA[$sKey] !== $oVarsB[$sKey])) {
+					$retAr[] = $sKey.' : '.(is_object($oVarsA[$sKey]) ? get_class($oVarsA[$sKey]) : $oVarsA[$sKey]).' <> '.(is_object($oVarsB[$sKey]) ? get_class($oVarsB[$sKey]) : $oVarsB[$sKey]);
+				}
+				if ($ignoretype && ($oVarsA[$sKey] != $oVarsB[$sKey])) {
+					$retAr[] = $sKey.' : '.(is_object($oVarsA[$sKey]) ? get_class($oVarsA[$sKey]) : $oVarsA[$sKey]).' <> '.(is_object($oVarsB[$sKey]) ? get_class($oVarsB[$sKey]) : $oVarsB[$sKey]);
+				}
+			}
+		}
+		return $retAr;
+	}
+}
---
+++ test/phpunit/RestAPIDocumentTest.php
@@ -0,0 +1,214 @@
+<?php
+/* Copyright (C) 2010 Laurent Destailleur  <eldy@users.sourceforge.net>
+ * Copyright (C) 2023 Alexandre Janniaux   <alexandre.janniaux@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * or see https://www.gnu.org/
+ */
+
+/**
+ *      \file       test/phpunit/RestAPIDocumentTest.php
+ *      \ingroup    test
+ *      \brief      PHPUnit test
+ *      \remarks    To run this script as CLI:  phpunit filename.php.
+ */
+global $conf,$user,$langs,$db;
+//define('TEST_DB_FORCE_TYPE','mysql');	// This is to force using mysql driver
+//require_once 'PHPUnit/Autoload.php';
+require_once dirname(__FILE__).'/../../htdocs/master.inc.php';
+require_once dirname(__FILE__).'/../../htdocs/core/lib/date.lib.php';
+require_once dirname(__FILE__).'/../../htdocs/core/lib/geturl.lib.php';
+require_once dirname(__FILE__).'/../../htdocs/core/lib/files.lib.php';
+require_once dirname(__FILE__).'/CommonClassTest.class.php';
+
+if (empty($user->id)) {
+	echo "Load permissions for admin user nb 1\n";
+	$user->fetch(1);
+	$user->getrights();
+}
+$conf->global->MAIN_DISABLE_ALL_MAILS = 1;
+$conf->global->MAIN_UMASK = '0666';
+
+/**
+ * Class for PHPUnit tests.
+ *
+ * @backupGlobals disabled
+ * @backupStaticAttributes enabled
+ * @remarks	backupGlobals must be disabled to have db,conf,user and lang not erased.
+ */
+class RestAPIDocumentTest extends CommonClassTest
+{
+	protected $api_url;
+	protected $api_key;
+
+	/**
+	 * setUpBeforeClass
+	 *
+	 * @return void
+	 */
+	public static function setUpBeforeClass(): void
+	{
+		global $conf,$user,$langs,$db;
+		$db->begin(); // This is to have all actions inside a transaction even if test launched without suite.
+
+		if (!isModEnabled('api')) {
+			print __METHOD__." module api must be enabled.\n";
+			die(1);
+		}
+
+		echo __METHOD__."\n";
+	}
+
+	/**
+	 * Init phpunit tests.
+	 *
+	 * @return void
+	 */
+	protected function setUp(): void
+	{
+		global $conf,$user,$langs,$db;
+		$conf = $this->savconf;
+		$user = $this->savuser;
+		$langs = $this->savlangs;
+		$db = $this->savdb;
+
+		$this->api_url = DOL_MAIN_URL_ROOT.'/api/index.php';
+
+		$login = 'admin';
+		$password = 'admin';
+		$url = $this->api_url.'/login?login='.$login.'&password='.$password;
+		// Call the API login method to save api_key for this test class.
+		// At first call, if token is not defined a random value is generated and returned.
+		$result = getURLContent($url, 'GET', '', 1, array(), array('http', 'https'), 2);
+		print __METHOD__." result = ".var_export($result, true)."\n";
+		print __METHOD__." curl_error_no: ".$result['curl_error_no']."\n";
+		$this->assertEquals($result['curl_error_no'], '');
+		$object = json_decode($result['content'], true);	// If success content is just an id, if not an array
+
+		$this->assertNotNull($object, "Parsing of json result must not be null");
+		$this->assertNotEquals(500, (empty($object['error']['code']) ? 0 : $object['error']['code']), 'Error'.(empty($object['error']['message']) ? '' : ' '.$object['error']['message']));
+		$this->assertEquals('200', $object['success']['code']);
+
+		$this->api_key = $object['success']['token'];
+
+		echo __METHOD__." api_key: $this->api_key \n";
+	}
+
+	/**
+	 * testPushDocument.
+	 *
+	 * @return int
+	 */
+	public function testPushDocument()
+	{
+		global $conf,$user,$langs,$db;
+
+		$url = $this->api_url.'/documents/upload?api_key='.$this->api_key;
+
+		echo __METHOD__.' Request POST url='.$url."\n";
+
+
+		// Send to non existent directory
+
+		dol_delete_dir_recursive(DOL_DATA_ROOT.'/medias/tmpphpunit/tmpphpunit1');
+
+		//$data = '{ "filename": "mynewfile.txt", "modulepart": "medias", "ref": "", "subdir": "mysubdir1/mysubdir2", "filecontent": "content text", "fileencoding": "" }';
+		$data = array(
+			'filename' => "mynewfile.txt",
+			'modulepart' => "medias",
+			'subdir' => "tmpphpunit/tmpphpunit1",
+			'filecontent' => "content text",
+			'fileencoding' => "",
+			'overwriteifexists' => 0,
+			'createdirifnotexists' => 0
+		);
+
+		$param = '';
+		foreach ($data as $key => $val) {
+			$param .= '&'.$key.'='.urlencode($val);
+		}
+
+		$result = getURLContent($url, 'POST', $param, 1, array(), array('http', 'https'), 2);
+		echo __METHOD__.' Result for sending document: '.var_export($result, true)."\n";
+		echo __METHOD__.' curl_error_no: '.$result['curl_error_no']."\n";
+		$object = json_decode($result['content'], true);
+		$this->assertNotNull($object, 'Parsing of json result must not be null');
+		$this->assertEquals('400', $result['http_code'], 'Test to push a document on a non existing dir does not return 400');
+		$this->assertEquals('400', (empty($object['error']['code']) ? '' : $object['error']['code']), 'Test to push a document on a non existing dir does not return 400');
+
+
+		// Send to existent directory
+
+		dol_delete_dir_recursive(DOL_DATA_ROOT.'/medias/tmpphpunit/tmpphpunit2');
+		dol_mkdir(DOL_DATA_ROOT.'/medias/tmpphpunit/tmpphpunit2');
+
+		$data = array(
+			'filename' => "mynewfile.txt",
+			'modulepart' => "medias",
+			'ref' => "",
+			'subdir' => "tmpphpunit/tmpphpunit2",
+			'filecontent' => "content text",
+			'fileencoding' => "",
+			'overwriteifexists' => 0,
+			'createdirifnotexists' => 0
+		);
+
+		$param = '';
+		foreach ($data as $key => $val) {
+			$param .= '&'.$key.'='.urlencode($val);
+		}
+
+		$result2 = getURLContent($url, 'POST', $param, 1, array(), array('http', 'https'), 2);
+		echo __METHOD__.' Result for sending document: '.var_export($result2, true)."\n";
+		echo __METHOD__.' curl_error_no: '.$result2['curl_error_no']."\n";
+		$object2 = json_decode($result2['content'], true);
+		//$this->assertNotNull($object2, 'Parsing of json result must not be null');
+		$this->assertEquals('200', $result2['http_code'], 'Return code must be 200');
+		$this->assertEquals($result2['curl_error_no'], '');
+		$this->assertEquals($object2, 'mynewfile.txt', 'Must contains basename of file');
+
+
+		dol_delete_dir_recursive(DOL_DATA_ROOT.'/medias/tmpphpunit/tmpphpunit3');
+
+		$data = array(
+			'filename' => "mynewfile.txt",
+			'modulepart' => "medias",
+			'ref' => "",
+			'subdir' => "tmpphpunit/tmpphpunit3",
+			'filecontent' => "content text",
+			'fileencoding' => "",
+			'overwriteifexists' => 0,
+			'createdirifnotexists' => 1
+		);
+
+		$param = '';
+		foreach ($data as $key => $val) {
+			$param .= '&'.$key.'='.urlencode($val);
+		}
+
+		$result3 = getURLContent($url, 'POST', $param, 1, array(), array('http', 'https'), 2);
+		echo __METHOD__.' Result for sending document: '.var_export($result3, true)."\n";
+		echo __METHOD__.' curl_error_no: '.$result3['curl_error_no']."\n";
+		$object3 = json_decode($result3['content'], true);
+		//$this->assertNotNull($object2, 'Parsing of json result must not be null');
+		$this->assertEquals('200', $result3['http_code'], 'Return code must be 200');
+		$this->assertEquals($result3['curl_error_no'], '');
+		$this->assertEquals($object3, 'mynewfile.txt', 'Must contains basename of file');
+
+
+		dol_delete_dir_recursive(DOL_DATA_ROOT.'/medias/tmpphpunit');
+
+		return 0;
+	}
+}
